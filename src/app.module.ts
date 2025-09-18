b87156d1b18e1fb6fc1e71a9b405b02f6be576d3
zarasah <zarinasah1989@gmail.com>
2025-09-18 14:00:41 +0400

    initial configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as process from "process";
+import {ConfigModule} from "@nestjs/config";
+import config from "./configs/config";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+      expandVariables: true,
+      load: [config],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
